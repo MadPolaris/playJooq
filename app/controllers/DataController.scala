5a9a4d9a79f4aca0237f194fadd402e7c0930ca9
Barry Zhong <template.polaris@gmail.com>
2015-01-01 23:53:18 +0800

    add a simple Future version of  action

--- app/controllers/DataController.scala
+++ app/controllers/DataController.scala
@@ -3,13 +3,18 @@ package controllers
 import models.Datas
 import play.api.mvc.{Action, Controller}
 
+import scala.concurrent.Future
+
 /**
- * Created by geek on 12/7/14.
+ * Created by Barry on 12/7/14.
  */
 object DataController extends Controller{
 
-
   def list = Action {
     Ok(views.html.data(Datas.listAll))
+  }
+
+  def futureList = Action.async {
+    Future {Datas.listAll} map { data => Ok(views.html.data(data)) }
   }
 }
