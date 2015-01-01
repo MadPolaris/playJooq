184e199ee21cca18379b868ed063d36d4e0c45d3
Barry Zhong <template.polaris@gmail.com>
2015-01-01 23:53:31 +0800

    remove unneccessary class

--- app/DBEvolve.scala
+++
@@ -1,8 +0,0 @@
-import java.io.File
-
-/**
- * Created by geek on 12/7/14.
- */
-object DBEvolve extends App {
-  play.api.db.evolutions.OfflineEvolutions.applyScript(new File("."), this.getClass.getClassLoader, "default")
-}
